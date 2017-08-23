50c6aac8a6e6d76a793f02b2862ff2e5729c23de
Kevin <kevin.tabesh@gmail.com>
2017-08-23 07:25:19 -0500

    Sport-App

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -1,10 +1,6 @@
 var navigation = "none";
 
 
-
-var cat = localStorage.getItem("myUser");
-console.log(cat);
-
 //navigation button functions
 $(function () {
   $('#bikes').on('click', function () {
@@ -63,7 +59,7 @@ $("#mapBtn").click(function() {
 
 //===============
  $.get("/api/routes", function(data) {
-     console.log(data);
+//     console.log(data);
      if (data != null){
          for(var i = 0; i< data.length; i++){
              var well = $("<div>");
@@ -72,19 +68,61 @@ $("#mapBtn").click(function() {
                           "<div class='col-xs-3' style = 'text-align:center;'>"+
                           "<p><strong>"+ data[i].routeName+"</strong>"+
                           "<p><strong>"+ data[i].routeDistance+"</strong>"+
-                          "<p><strong>"+ data[i].userLocation+"</strong></div>"+
-                          "<div class='col-xs-6' style = 'text-align:center;'>IMAGE</div></div>"
+                          "<p><strong>"+ data[i].routeLocation+"</strong></div>"+
+                          "<div class='col-xs-6' style = 'text-align:center;'><img class='images' data-toggle='modal' data-target='#exampleModalLong' id='"+data[i].routeImageLg+"' src="+data[i].routeImageSm+" style='height: 70%; width: 70%; object-fit: contain'></div>"+
+                          "<div class='col-xs-3' style = 'text-align:center;'><button type='submit' class='btn btn-default' id ='routeSubmit'>Add this route</button></div></div>"
                          );
              
             $("#theRoutes").append(well);
          }
      }
   });
+
+var cat = localStorage.getItem("myUser");
+console.log("CAT: " + cat); 
+
+function checkUserRoutes(cat){
+
+	$.get("/api/userRoutes/"+ cat , function(data) {
+      console.log("hello");
+      userRoutes(data);
+    });
+}
+
+function userRoutes(data){
+    if (data != null){
+         for(var i = 0; i< data.length; i++){
+             console.log(data[i]);
+             var well = $("<div>");
+              well.addClass("well");
+              well.append("<div class='row'>"+
+                          "<div class='col-xs-3' style = 'text-align:center;'>"+
+                          "<p><strong>"+ data[i].routeName+"</strong>"+
+                          "<p><strong>"+ data[i].routeDistance+"</strong>"+
+                          "<p><strong>"+ data[i].routeLocation+"</strong></div>"+
+                          "<div class='col-xs-6' style = 'text-align:center;'><img class='images' data-toggle='modal' data-target='#exampleModalLong' id='"+data[i].routeImageLg+"' src="+data[i].routeImageSm+" style='height: 50%; width: 50%; object-fit: contain'></div>"+
+                          "<div class='col-xs-3' style = 'text-align:center;'><button type='submit' class='btn btn-default' id ='routeSubmit'>Delete route</button></div></div>"
+                         );
+             
+            $(".myRouteBody").append(well);
+         }
+     }else{
+         $(".myRouteBody").append("<p>You have not chosen any route</p>");
+     }
+}
+
+$(document).on("click", ".images", function(event){
+    var img = $("<div>");
+    img.append("<img src='"+this.id+"'style='height: 100%; width: 100%; object-fit: contain'>");
+    $(".modal-body").append(img);
+});
+$(document).on("click", ".close", function(event){
+    $(".modal-body").empty();
+});
+
+
 //==================
 
-//chatroom submit clicked
-
-
 $("#chatSubmit").on("click",function(){
 
   event.preventDefault();
@@ -95,19 +133,14 @@ $("#chatSubmit").on("click",function(){
   var groupName = $("#group").val().trim();
 
   insertPost(userName,userMessage,groupName);
+//    clearFields();
 });
 
-$("#chatSubmit").on("click",function(){
-
-  event.preventDefault();
-  console.log("New Chat message");
-
-  var userName = $("#username").val().trim();
-  var userMessage = $("#message").val().trim();
-  var groupName = $("#group").val().trim();
-
-  insertPost(userName,userMessage,groupName);
-});
+//function clearFields(){
+//    $("#username").val()="";
+//    $("#message").val()="";
+//    
+//}
 
 function insertPost(userName, userMessage, groupName) {
     //working
@@ -154,7 +187,7 @@ function appendData(data){
 
       var heading = $("<div>");
       heading.addClass("panel-heading");
-      heading.append("<h2>" + data[i].title + " sent " + moment(data[i].created_at).format("h:mma on dddd") + " to " + data[i].group + "<h2>")
+      heading.append("<h4><strong>" + data[i].title + "</strong> sent " + moment(data[i].created_at).format("h:mma on dddd") + " to <strong>" + data[i].group + "</strong></h4>")
       row.append(heading);
 
       var body = $("<div>");
@@ -171,6 +204,5 @@ function appendData(data){
 }
 
 
-
-
 checkPosts();
+checkUserRoutes(cat);
